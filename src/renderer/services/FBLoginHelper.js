737a3e69c1aa4ae9817bf8478726a37acaf721f0
Ali Can Bardakci <alicanbardakci@gmail.com>
2017-06-08 08:02:55 -0400

    FB login issue fixed #1

--- src/renderer/services/FBLoginHelper.js
+++ src/renderer/services/FBLoginHelper.js
@@ -2,7 +2,7 @@ import Request from 'request'
 import Api from './Api'
 
 const BrowserWindow = window.require('electron').remote.BrowserWindow
-const loginUrl = 'https://www.facebook.com/v2.6/dialog/oauth?redirect_uri=fb464891386855067%3A%2F%2Fauthorize%2F&state=%7B%22challenge%22%3A%22q1WMwhvSfbWHvd8xz5PT6lk6eoA%253D%22%2C%22com.facebook.sdk_client_state%22%3Atrue%2C%223_method%22%3A%22sfvc_auth%22%7D&scope=user_birthday%2Cuser_photos%2Cuser_education_history%2Cemail%2Cuser_relationship_details%2Cuser_friends%2Cuser_work_history%2Cuser_likes&response_type=token%2Csigned_request&default_audience=friends&return_scopes=true&auth_type=rerequest&client_id=464891386855067&ret=login&sdk=ios'
+const loginUrl = 'https://m.facebook.com/v2.8/dialog/oauth?auth_type=rerequest&client_id=464891386855067&default_audience=friends&display=touch&e2e=%7B%22init%22:1947351.737793%7D&fbapp_pres=0&redirect_uri=fb464891386855067://authorize/&response_type=token,signed_request&return_scopes=true&scope=user_birthday,user_photos,user_education_history,email,user_relationship_details,user_friends,user_work_history,user_likes&sdk=ios&sdk_version=4.20.2&state=%7B%22challenge%22:%22wt7ktcsTn66e3eXP5hkB6OiZHG0%253D%22,%220_auth_logger_id%22:%22A5AA6F71-41B8-4D04-9896-7E6A2FC26295%22,%22com.facebook.sdk_client_state%22:true,%223_method%22:%22sfvc_auth%22%7D&sfvc=1'
 
 let fbAuthData = {}
 let callback
@@ -20,7 +20,7 @@ var tinderLogin = function () {
 
 var checkForToken = function (loginWindow, interval, callback) {
   var url = loginWindow.getURL()
-  if (url.indexOf('https://m.facebook.com/v2.6/dialog/oauth/confirm') > -1) {
+  if (url.indexOf('https://m.facebook.com/v2.8/dialog/oauth/confirm') > -1) {
     loginWindow.webContents.executeJavaScript("unescape(document.getElementsByTagName('script')[0].innerHTML)", false,
       function (result) {
         clearInterval(interval)
